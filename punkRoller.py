06275d7ab142b9725cecde6cd2966a44c648f994
weisje <20269574+weisje@users.noreply.github.com>
2024-05-23 07:24:39 -0400

    created base frame for currently known functions, including cashAndGearRoller(), attributeRoller(), statsRoller(), weaponRoller(), armorRoller(), debtRoller(), appRoller(), nanoPowerRoller(), infestationRoller(),

--- punkRoller.py
+++ punkRoller.py
@@ -2,5 +2,41 @@ def main():
     pass
 
 
+def cashAndGearRoller():
+    pass
+
+
+def attributeRoller(preferredStat=False):
+    pass
+
+
+def statsRoller():
+    pass
+
+
+def weaponRoller():
+    pass
+
+
+def armorRoller():
+    pass
+
+
+def debtRoller():
+    pass
+
+
+def appRoller():
+    pass
+
+
+def nanoPowerRoller():
+    pass
+
+
+def infestationRoller():
+    pass
+
+
 if __name__ == '__main__':
     main()
