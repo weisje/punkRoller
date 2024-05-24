cda638229ec538f62dd3ca65a2ec57fc1c0a3b1d
weisje <20269574+weisje@users.noreply.github.com>
2024-05-24 07:27:32 -0400

    Populated csvReader().  Imported csv library.  Tested functionality.

--- punkRoller.py
+++ punkRoller.py
@@ -1,5 +1,9 @@
+import csv
+
+
 def main():
-    pass
+    testFile = csvReader("Documents\\cashAndGearColumn1.csv")
+    print(testFile)
 
 
 def cashAndGearRoller():
@@ -42,8 +46,27 @@ def infestationRoller():
     pass
 
 
-def csvReader(documentFilePath, hasHeader="True", delmiterCharacter = "|", ) -> list:
-    pass
+def csvReader(documentFilePath, hasHeader="True", delimiterChar = "|", ) -> list:
+    """
+    Function for reading in designated CSV files, populating contents of rows to a list, then returning a list of lists for said contents
+    :param documentFilePath: Document filepath where the csv file is located, including the csv file itself(i.e. "\\documents\\cashAndGearColumn1.csv"
+    :type documentFilePath: str
+    :param delimiterChar: Character defined as the seperator between columns within the csv file
+    :type: char
+    :param hasHeader: Specifies if the csv file has a header included for the information or not.
+    :type hasHeader: bool
+    :return: list
+    """
+    csvContentList = [] # Final list that will be returned to caller
+    with open(documentFilePath, 'r') as csvContentFile:
+        csvReadEngine = csv.reader(csvContentFile, delimiter=delimiterChar)
+        if hasHeader:
+            next(csvReadEngine)
+        for row in csvReadEngine:
+            csvContentList.append(row)
+    csvContentFile.close()
+
+    return csvContentList
 
 
 if __name__ == '__main__':
